9c14c2bfb407390cba2021edde9e6242c21196de
Francisco Ciordia Cantarella <gatupancho@gmail.com>
2024-01-13 22:05:17 -0300

    add: added remito pdf generator

--- remito_view/urls.py
+++ remito_view/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from .views import gestionar_remito, del_remito, \
-    gestionar_transacciones, del_transaccion
+    gestionar_transacciones, del_transaccion, generar_pdf_remito
 
 urlpatterns = [
     path('', gestionar_remito, name='gestionar_remito'),
@@ -10,6 +10,8 @@ urlpatterns = [
          name='gestionar_transacciones'),
     path('<int:nro_remito>/del_transaccion/<int:transaccion_id>',
          del_transaccion, name='del_transaccion'),
+    path('generar_pdf_remito/<int:remito_id>/',
+         generar_pdf_remito, name='generar_pdf_remito'),
     # path('del_articulo/<int:articulo_id>/',
     # del_articulo, name='del_articulo'),
     # ... otras rutas
--- remito_view/views.py
+++ remito_view/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .models import Remito, Transaccion
 from .forms import RemitoForm, TransaccionForm
 
+from django.http import HttpResponse
+from weasyprint import HTML
+
 # Create your views here.
 # views.py
 
@@ -63,3 +66,24 @@ def del_transaccion(request, nro_remito, transaccion_id):
 
     transaccion.delete()
     return redirect('gestionar_transacciones', nro_remito)
+
+
+def generar_pdf_remito(request, remito_id):
+    remito = get_object_or_404(Remito, id=remito_id)
+    transacciones = Transaccion.objects.filter(
+        remito=remito).order_by('-articulo__name')
+
+    # Renderiza el HTML utilizando una plantilla
+    html_string = render(request, 'remito_template.html', {
+                         'transacciones': transacciones,
+                         'remito': remito}).content.decode('utf-8')
+
+    # Convierte el HTML a PDF usando WeasyPrint
+    pdf_file = HTML(string=html_string,
+                    base_url=request.build_absolute_uri()).write_pdf()
+
+    # Prepara la respuesta HTTP con el archivo PDF
+    response = HttpResponse(pdf_file, content_type='application/pdf')
+    response['Content-Disposition'] = 'filename="remito.pdf"'
+
+    return response
