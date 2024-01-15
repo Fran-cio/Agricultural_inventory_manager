2e0cd1edd6aea9b03eaba6ebca3567b997bee5ff
Francisco Ciordia Cantarella <gatupancho@gmail.com>
2024-01-15 16:56:30 -0300

    conf: deploy conf

--- toro_proyect/settings.py
+++ toro_proyect/settings.py
@@ -23,11 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6-r^&$l@d1d^%1lfrzg^5ztc#s4*x+-iesc09kc@m!^4r^vw8d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-# False
+DEBUG = False
 
 ALLOWED_HOSTS = [
-    # '192.168.10.121'
+    '192.168.10.116'
 ]
 
 
